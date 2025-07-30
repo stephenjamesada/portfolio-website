795d847f5b07eb58180e4c740b1f445ba2892f58
stephenjamesada <ada_stephen@proton.me>
2025-07-30 15:19:49 -0500

    Handle 404.html + more existing tweaks

--- main.py
+++ main.py
@@ -14,5 +14,9 @@ def about():
 def hardware():
     return render_template('hardware.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
