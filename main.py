1b9d2e4b62c0134ed0b08158cbe9cf1809a9f21e
stephenjamesada <ada_stephen@proton.me>
2025-07-31 00:13:49 -0500

    Added 500.html, config.py

--- main.py
+++ main.py
@@ -18,5 +18,9 @@ def hardware():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
