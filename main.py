917383a845900e4c3eb1dbbf560c56cc937b0cc1
stephenjamesada <ada_stephen@proton.me>
2025-08-08 02:22:39 -0500

    🐛 Unterminated string literal in main.py on SSL config line

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.config['SECRET_KEY'] = SECRET_KEY
 app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER", 'smtp.gmail.com')
 app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT", 587))
 app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS", "true").lower() == "true"
-app.config['MAIL_USE_SSL'] = os.getenv("MAIL_USE_SSL", "false").lower() == true"
+app.config['MAIL_USE_SSL'] = os.getenv("MAIL_USE_SSL", "false").lower() == "true"
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_DEFAULT_SENDER", app.config['MAIL_USERNAME'])
