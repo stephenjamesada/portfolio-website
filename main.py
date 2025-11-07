046ed31c2654c34ce09e54f880e1a72ca8f56ae5
stephenjamesada <ada_stephen@proton.me>
2025-11-07 12:15:53 -0600

    🐛 Forgot to add the host in main.py

--- main.py
+++ main.py
@@ -55,4 +55,4 @@ def service_unavailable(e):
     return render_template('503.html'), 503
     
 if __name__ == "__main__":
-    app.run(port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
